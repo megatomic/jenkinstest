0f2dbf58a6db18776313fc0a606305a047edd6aa
yspark <megatomic@naver.com>
2022-07-12 12:46:59 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+	res.send('hello world');
+})
+
+app.listen(3500,()=>{
+	console.log('listening on 3500');
+});
+
+module.exports = app;
